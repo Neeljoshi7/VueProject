6217409ac43b12ba57e88bbe290fe9cb44e3d839
Neel <njoshi4433@gmail.com>
2022-11-17 19:04:42 +0530

    Create punch bage game

---
+++ src/components/CustomNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <b-navbar type="dark" variant="light">
+        <b-navbar-nav>
+          <RouterLink class="me-2" to="/">PunchBag</RouterLink>
+          <RouterLink class="me-2" to="/about">About</RouterLink>
+        </b-navbar-nav>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+export default {
+  name: "CustomNavbar",
+};
+</script>
+
+<style scoped>
+a{
+  color: gray;
+  text-decoration: none;
+}
+</style>
---
+++ src/views/PunchBag.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>
+        <div id="bag" v-bind:class="{brust : ended}">
+             <img v-if="ended" src="../assets/bag-burst.png" alt="punch">
+            <img  v-else src="../assets/bag.png" alt="punch Image">
+           
+        </div>
+        <div id="bag-health" >
+            <div v-bind:style="{width : health + '%'}"></div>
+        </div>
+        <div id="controls">
+            <button @click="punch" v-show="!ended" >Punch</button>
+            <button @click="restart">Restart</button>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data()
+    {
+        return{
+        
+        health:100,
+        ended:false,
+        }
+    },
+    methods:{
+        punch()
+        {
+            this.health -= 10;
+            if (this.health <=0 ) {
+            this.ended = true
+            }
+            
+        },
+        restart()
+        {
+            this.health = 100;
+        }
+
+    }
+}
+</script>
+
+<style scoped src="../assets/css/style.css"></style>
